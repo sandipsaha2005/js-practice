5e7362028f7dcd1351fca7034cca35e1ef964067
sandip <iamsandipsaha2005@gmail.com>
2025-09-27 08:13:57 +0530

    modified folder strucuter

---
+++ week_2/day_8/function.js
@@ -0,0 +1,11 @@
+function printSomeThing(message) {
+    console.log(message);
+}
+
+function floorValue(x) {
+    const remainder = x % 2;
+    return x - remainder;
+}
+
+console.log(floorValue(12.34));
+ 
